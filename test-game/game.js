29ed6a717ab3ac932268bfb4dd553b7306157cd7
alexmdodge <alex.mdodge@gmail.com>
2016-08-01 09:26:23 -0300

    Added classes for player and ball. Project will be coded in ES6, then use the babel transpiler to bring it back to regular standards.

--- test-game/game.js
+++ test-game/game.js
@@ -3,6 +3,34 @@
 /* Grid Grind Test */
 /* Below is some pseudo code and ideas for the games interactions */
 
+/* Player will be a singleton pattern so cannot create multiple players */
+object player {
+   var player = "#player";
+   var life, posX, posY, isCreated;
+
+   constructor = function() {
+      life = 5;
+      posX = $(player).getPosX;
+      posY = $(player).getPosY;
+      isCreated = false;
+   }
+}
+
+/* Balls are constructed with the class selector, as there will eventually 
+   be multiples */
+object ball {
+   var ball = ".ball";
+   var posX, posY, startX;
+
+   constructor = function() {
+      posX = $(ball).getPosX;
+      posY = $(ball).getPosY;
+   }
+
+   genStart = function(posX) {
+      var choice = Math.floor(Math.random());
+   }
+}
 
 /* Creates a player and fades in the element.
  *
@@ -19,13 +47,15 @@ function initPlayer() {
 
 function initBall() {
    var ball = '#ball';
+   // var currentPosY = player.getPosY();
    var ballIsFalling = true;
    var ballHasHit = false;
    var ballHeight = 100;
    $(ball).delay(1000).fadeIn('slow');
 
    while(ballIsFalling) {
-      // Move ball down 10px at a time
+      // Move ball down 10px at a time, function drops ball
+      // ball.fall(); 
       $(ball).delay(200).css('top:110px;');
 
       //
