e3138a199da201493f08b1b8e85e532bc9e479f2
alexmdodge <alex.mdodge@gmail.com>
2016-08-27 16:51:38 -0300

    Test Game Completed Added collisions with the paddle. Added brick generation and collision detection. Added support for lives and score keeping.

--- test-game/game.js
+++ test-game/game.js
@@ -4,14 +4,12 @@
 var canvas = document.getElementById("my-canvas");
 var ctx = canvas.getContext("2d");
 
+var score = 0;
+var lives = 3;
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
-
-// Defines colours for random color change
-var colors = ["red", "blue", "green", "yellow", "black", "orange"];
-var curColor = "green"; 
 
 // Defines the dimensions for the ball and paddle
 var ballRadius = 10;
@@ -23,51 +21,32 @@ var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
 
-function drawBall() {
-   ctx.beginPath();
-   ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-   ctx.fillStyle = curColor;
-   ctx.fill();
-   ctx.closePath();
-}
+// Variables and dimensions for the destroyable bricks
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
 
-function drawPaddle() {
-    ctx.beginPath();
-    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
-}
-
-function draw() {
-   ctx.clearRect(0, 0, canvas.width, canvas.height);
-   drawBall();
-   drawPaddle();
-
-   x += dx;
-   y += dy;
-
-   if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-      dx = -dx;
-      curColor = colors[(Math.floor(Math.random() * 6))];
-
-   }
-
-   if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-      dy = -dy;
-      curColor = colors[(Math.floor(Math.random() * 6))];
-
-   }
-
-   if(rightPressed && paddleX < canvas.width-paddleWidth) {
-      paddleX += 5;
-   } else if(leftPressed && paddleX > 0) {
-      paddleX -= 5;
+var bricks = [];
+for(var c=0; c<brickColumnCount; c++) {
+   bricks[c] = [];
+   for(var r=0; r<brickRowCount; r++) {
+      bricks[c][r] = { x: 0, y: 0, hit: false };
    }
 }
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ *                            Event Listeners
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
 
 function keyDownHandler(e) {
     if(e.keyCode == 39) {
@@ -87,4 +66,136 @@ function keyUpHandler(e) {
     }
 }
 
-setInterval(draw, 10);
+function mouseMoveHandler(e) {
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if(relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth/2;
+    }
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ *                            Game Functions
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+function drawLives() {
+   ctx.font = "16px Helvetica";
+   ctx.fillStyle = "#0095DD";
+   ctx.fillText("Lives: "+lives, canvas.width-65, 20);
+}
+
+function drawScore() {
+    ctx.font = "16px Helvetica";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: "+score, 8, 20);
+}
+
+function drawBall() {
+   ctx.beginPath();
+   ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+   ctx.fillStyle = "black";
+   ctx.fill();
+   ctx.closePath();
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawBricks() {
+   for(var c=0; c<brickColumnCount; c++) {
+      for(var r=0; r<brickRowCount; r++) {
+         if (!bricks[c][r].hit) {
+            var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+            var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+            bricks[c][r].x = brickX;
+            bricks[c][r].y = brickY;
+            ctx.beginPath();
+            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+         }
+      }
+   }
+}
+
+function collisionDetected() {
+   for(var c=0; c<brickColumnCount; c++) {
+      for(var r=0; r<brickRowCount; r++) {
+         var b = bricks[c][r];
+         if (y > b.y && y < b.y + brickHeight && x > b.x && x < b.x + brickWidth && !b.hit) {
+            bricks[c][r].hit = true;
+            dy = -dy;
+            score++;
+
+            if(score == brickRowCount*brickColumnCount) {
+               alert("YOU WIN, CONGRATULATIONS!");
+               document.location.reload();
+            }
+         }
+      }
+   }
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ *                            Game Driver
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+function draw() {
+   ctx.clearRect(0, 0, canvas.width, canvas.height);
+   drawScore();
+   drawLives();
+   drawBall();
+   drawBricks();
+   drawPaddle();
+
+   x += dx;
+   y += dy;
+
+   if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+      dx = -dx;
+   }
+
+   collisionDetected();
+
+   if (y + dy < ballRadius) {
+      dy = -dy;
+   } else if(y + dy > canvas.height-ballRadius) {
+      if(x > paddleX && x < paddleX + paddleWidth) {
+         dy = -dy;
+      } else {
+         lives--;
+         if(!lives) {
+             alert("GAME OVER");
+             document.location.reload();
+         }
+         else {
+             x = canvas.width/2;
+             y = canvas.height-30;
+             dx = 2;
+             dy = -2;
+             paddleX = (canvas.width-paddleWidth)/2;
+         }
+      }
+   }
+
+   if(rightPressed && paddleX < canvas.width-paddleWidth) {
+      paddleX += 5;
+   } else if(leftPressed && paddleX > 0) {
+      paddleX -= 5;
+   }
+
+   requestAnimationFrame(draw);
+}
+
+
+
+draw();
