b8f81a09b3a1800fedfb7afbbb80ce0990e220be
alexmdodge <alex.mdodge@gmail.com>
2016-09-01 23:25:16 -0300

    Updated Game Setup Worked on configuring the color objects to be rendered randomly to the screen. Might start by first rendering test objects to the screen, then creating random objects to interact with after that. Could also generate the objects as buttons. This also organizes the global variables which will eventually be refactored for best practice.

--- app/js/game.js
+++ app/js/game.js
@@ -1,4 +1,5 @@
 'use strict';
+/* globals Phaser, console */
 
 var game = new Phaser.Game(500, 500, Phaser.AUTO, null, {
    preload: preload, 
@@ -16,6 +17,9 @@ var game = new Phaser.Game(500, 500, Phaser.AUTO, null, {
 //
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+// Debug Messages
+var inDebug = true;
+var debugTemp;
 
 // Blocks
 var redBlock;
@@ -25,38 +29,29 @@ var yellowBlock;
 var orangeBlock;
 var purpleBlock;
 
+var gridColors = [];
+var blocks;
+var blockInfo;
+var blockX = 0;
+var blockY = 0;
+
 // Information
-var movesLeft;
-var name;
+var movesLeft = 3;
+var movesText;
+var playerName;
 var score = 0;
+var scoreText;
+
+var startGame;
 var startButton;
 
 // Grid Variables
-var levelRow;
-var levelCol;
+var gridSize = 9;
+var levelRow = Math.sqrt(gridSize);
+var levelCol = Math.sqrt(gridSize);
 
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-//
-//
-//                         Old Game Variables
-//
-//
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-var ball;
-var bricks;
-var newBrick;
-var paddle;
-var brickInfo;
-
+// Styling Variables
 var textStyle = { font: '18px Helvetica', fill: '#0095DD' };
-var scoreText;
-
-var lives = 3;
-var livesText;
-var lifeLostText;
-
-var playing = false;
 
 function preload() {
    // Scales the game to the screen size
@@ -65,12 +60,15 @@ function preload() {
    game.scale.pageAlignVertically = true;
    game.stage.backgroundColor = '#eee';
 
-   game.load.image('redB', '../img/redBlock.png');
-   game.load.image('greenB', '../img/greenBlock.png');
-   game.load.image('blueB', '../img/blueBlock.png');
+   game.load.image('redB',    '../img/redBlock.png');
+   game.load.image('greenB',  '../img/greenBlock.png');
+   game.load.image('blueB',   '../img/blueBlock.png');
    game.load.image('yellowB', '../img/yellowBlock.png');
    game.load.image('orangeB', '../img/orangeBlock.png');
    game.load.image('purpleB', '../img/purpleBlock.png');
+
+   // Generates the random grig as needed
+   generateGrid();
 
    // Example spritsheet for animations
    game.load.spritesheet('button', '../img/button.png', 120, 40);
@@ -80,46 +78,60 @@ function create() {
    startButton = game.add.button(game.world.width*0.5, game.world.height*0.5, 'button', startGame, this, 1, 0, 2);
    startButton.anchor.set(0.5);
    
-   redBlock = game.add.sprite(50, 250, 'ball');
-   ball.animations.add('wobble', [0,1,0,2,0,1,0,2,0], 24);
+   // Initialize all of the color object sprites
+   redBlock    = game.add.sprite(50, 250, 'redB');
+   greenBlock  = game.add.sprite(50, 250, 'greenB');
+   blueBlock   = game.add.sprite(50, 250, 'blueB');
+   yellowBlock = game.add.sprite(50, 250, 'yellowB');
+   orangeBlock = game.add.sprite(50, 250, 'orangeB');
+   purpleBlock = game.add.sprite(50, 250, 'purpleB');
 
-   ball.anchor.set(0.5);
-   paddle.anchor.set(0.5,1);
+   // Sets the anchor coordinate to be the center of the sprite
+   redBlock.anchor.set(0.5);
+   greenBlock.anchor.set(0.5);
+   blueBlock.anchor.set(0.5);
+   yellowBlock.anchor.set(0.5);
+   orangeBlock.anchor.set(0.5);
+   purpleBlock.anchor.set(0.5);
+   
 
-   paddle.scale.setTo(0.35, 0.35);
-
+   // Enables physics for the game
    game.physics.startSystem(Phaser.Physics.ARCADE);
-   game.physics.enable(ball, Phaser.Physics.ARCADE);
-   game.physics.enable(paddle, Phaser.Physics.ARCADE);
+   // game.physics.enable(ball, Phaser.Physics.ARCADE);
 
+   // Draws the block objects on the screen for each frame
+   // Draws from a randomized array of the original sprite colours
+   initBlocks();
 
-   ball.body.collideWorldBounds = true;
-   game.physics.arcade.checkCollision.down = false;
-   ball.checkWorldBounds = true;
-
-   ball.events.onOutOfBounds.add(ballLeaveScreen, this);
-
-   ball.body.bounce.set(1);
-   paddle.body.immovable = true;
-
-   initBricks();
-
-   scoreText = game.add.text(5, 5, 'Points: 0', textStyle);
-   livesText = game.add.text(game.world.width-5, 5, 'Lives: '+lives, textStyle);
-   livesText.anchor.set(1,0);
-   lifeLostText = game.add.text(game.world.width*0.5, game.world.height*0.5, 'Life lost, click to continue', textStyle);
-   lifeLostText.anchor.set(0.5);
-   lifeLostText.visible = false;
-
+   scoreText = game.add.text(5, 5, 'Points: ' + score, textStyle);
+   movesText = game.add.text(game.world.width-5, 5, 'Moves Left: '+ movesLeft, textStyle);
+   movesText.anchor.set(1,0);
 }
 
 function update() {
 }
 
-function initBricks() {
-   brickInfo = {
-      width: 65,
-      height: 20,
+function generateGrid() {
+   var colors = [redBlock, greenBlock, blueBlock, yellowBlock, orangeBlock, purpleBlock];
+
+   for (var r = 0; r < levelRow; r++) {
+
+      gridColors[r] = [];
+      for (var c = 0; c < levelCol; c++) {
+         gridColors[r][c] = colors[Math.floor(Math.random*6)];
+
+         if(inDebug) {
+            debugTemp = gridColors[r][c];
+            console.log("" + debugTemp);
+         }
+      }
+   }
+}
+
+function initBlocks() {
+   blockInfo = {
+      width: 25,
+      height: 25,
       count: {
          row: levelRow,
          col: levelCol
@@ -131,23 +143,35 @@ function initBricks() {
       padding: 10
    };
 
-   bricks = game.add.group();
+   blocks = game.add.group();
    
-   for(var c=0; c<brickInfo.count.row; c++) {
-      for(var r=0; r<brickInfo.count.col; r++) {
-         var blockX = (r*(brickInfo.width+brickInfo.padding))+brickInfo.offset.left;
-         var blockY = (c*(brickInfo.height+brickInfo.padding))+brickInfo.offset.top;
-         newBrick = game.add.sprite(brickX, brickY, 'brick');
+   for(var r=0; r < levelRow; r++) {
+      for(var c=0; c < levelCol; r++) {
+         blockX = (r*(blockInfo.width+blockInfo.padding))+blockInfo.offset.left;
+         blockY = (c*(blockInfo.height+blockInfo.padding))+blockInfo.offset.top;
+         newBrick = game.add.sprite(blockX, blockY, 'brick');
          newBrick.scale.setTo(0.18, 0.18);
 
 
          game.physics.enable(newBrick, Phaser.Physics.ARCADE);
          newBrick.body.immovable = true;
          newBrick.anchor.set(0.5);
-         bricks.add(newBrick);
+         blocks.add(newBrick);
       }
    }
 }
+
+function startGame() {
+   startButton.destroy();
+   blocks.setVisibility = true;
+   playing = true;
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ *          Example Function of using transition animations
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 function ballHitBrick(ball, brick) {
    var killTween = game.add.tween(brick.scale);
@@ -161,37 +185,10 @@ function ballHitBrick(ball, brick) {
    score += 10;
    scoreText.setText('Points: '+score);
 
-   if(score === brickInfo.count.row*brickInfo.count.col*10) {
+   if(score === blockInfo.count.row*blockInfo.count.col*10) {
       alert('You won the game, congratulations!');
       location.reload();
    }
 }
 
-function ballLeaveScreen() {
-    lives--;
-    if(lives) {
-      livesText.setText('Lives: '+lives);
-      lifeLostText.visible = true;
-      ball.reset(game.world.width*0.5, game.world.height-25);
-      paddle.reset(game.world.width*0.5, game.world.height-5);
-      
-      game.input.onDown.addOnce(function(){
-         lifeLostText.visible = false;
-         ball.body.velocity.set(150, -150);
-      }, this);
-   } else {
-       alert('You lost, game over!');
-       location.reload();
-   }
-}
-
-function ballHitPaddle(ball, paddle) {
-   ball.animations.play('wobble');
-   ball.body.velocity.x = -1*5*(paddle.x-ball.x);
-}
-
-function startGame() {
-   startButton.destroy();
-   ball.body.velocity.set(150, -150);
-   playing = true;
-}
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
