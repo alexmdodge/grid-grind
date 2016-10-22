b297a64442fd8d6b4e4f2971ac21724f6df1b92b
alexmdodge <alex.mdodge@gmail.com>
2016-10-22 12:36:01 -0300

    Convert levels object Convert the levels object so it is written in ES 2015+ format. This will then lead more naturally into the test cases. This can now be treated more naturally as an exported object.

--- app/js/levels.js
+++ app/js/levels.js
@@ -1,4 +1,4 @@
-'use strict';
+/* jshint esversion: 6 */
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *  
@@ -20,66 +20,67 @@
  *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-/* Constructor */
-function Level(currentLevel, gameSize, spriteSize) {
-	this.currentLevel = currentLevel;
-	this.gameSize = gameSize;
-	this.spriteSize = spriteSize;
+export class Level {
+
+	/* Constructor */
+	constructor(currentLevel, gameSize, spriteSize) {
+		this.currentLevel = currentLevel;
+		this.gameSize = gameSize;
+		this.spriteSize = spriteSize;
+	}
+
+	/*
+	 * getGridSize
+	 *
+	 * Retrieves the grid size for the level. The first level is a 3x3 and it increases
+	 * by perfect squares each level, with some levels increasing only point value. The
+	 * pattern for generation will change depending on how quickly the levels can be played.
+	 */
+	getGridSize() {
+		return this.currentLevel + 2; // If level 1, 3 blocks per row, 3 columns
+	}
+
+	/*
+	 * getMoves
+	 *
+	 * Returns the number of moves available to the player
+	 */
+	getMoves() {
+		return this.currentLevel * 2 + 3; // Increase the moves each level by 4
+	}
+
+	/*
+	 * getMoves
+	 *
+	 * Returns the number of points to complete the leve
+	 */
+	getPoints() {
+		return this.currentLevel * this.currentLevel + 4; // Square points required
+	}
+
+	/*
+	 * getBlockSize
+	 *
+	 * Returns the side length of the block for the current level size.
+	 * First divides the game size into chunks according to the grid size
+	 * then the padding is subtracted from each to account for it in the drawing
+	 * function.
+	 */
+	getBlockSize() {
+		var size = Math.floor( (this.gameSize / this.getGridSize()) - 5); 
+		return size;
+	}
+
+	/*
+	 * getBlockScale
+	 *
+	 * The source image for the spritesheet determines the physical pizel size
+	 * of the blocks. This function determines what scale factor is needed for
+	 * the size of the game. Will change dynamically depending of game size, even
+	 * when scaled.
+	 */
+	getBlockScale() {
+		var scale = this.getBlockSize() / this.spriteSize; 
+		return scale;
+	}
 }
-
-/*
- * getGridSize
- *
- * Retrieves the grid size for the level. The first level is a 3x3 and it increases
- * by perfect squares each level, with some levels increasing only point value. The
- * pattern for generation will change depending on how quickly the levels can be played.
- */
-Level.prototype.getGridSize = function() {
-	return this.currentLevel + 2; // If level 1, 3 blocks per row, 3 columns
-};
-
-
-/*
- * getMoves
- *
- * Returns the number of moves available to the player
- */
-Level.prototype.getMoves = function() {
-	return this.currentLevel * 2 + 3; // Increase the moves each level by 4
-};
-
-/*
- * getMoves
- *
- * Returns the number of points to complete the leve
- */
-Level.prototype.getPoints = function() {
-	return this.currentLevel * this.currentLevel + 4; // Square points required
-};
-
-/*
- * getBlockSize
- *
- * Returns the side length of the block for the current level size.
- * First divides the game size into chunks according to the grid size
- * then the padding is subtracted from each to account for it in the drawing
- * function.
- */
-Level.prototype.getBlockSize = function() {
-	var size = Math.floor( (this.gameSize / this.getGridSize()) - 5); 
-	return size;
-};
-
-
-/*
- * getBlockScale
- *
- * The source image for the spritesheet determines the physical pizel size
- * of the blocks. This function determines what scale factor is needed for
- * the size of the game. Will change dynamically depending of game size, even
- * when scaled.
- */
-Level.prototype.getBlockScale = function() {
-	var scale = this.getBlockSize() / this.spriteSize; 
-	return scale;
-};
